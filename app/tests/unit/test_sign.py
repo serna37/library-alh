57e9166b4a4850af88ac6577e1c1b4403a0f6b67
serna37 <serna37@serna37noMacBook-Pro.local>
2023-10-29 15:41:15 +0900

    Modify useless empty char

--- app/tests/unit/test_sign.py
+++ app/tests/unit/test_sign.py
@@ -3,14 +3,14 @@ import base64
 from src.service import sign
 
 def test_signup_hash_password_same():
-    salt = base64.b64encode(os.urandom(32)) 
+    salt = base64.b64encode(os.urandom(32))
     plain = 'test-password1'
     rv1 = sign._hash_password(plain, salt)
     rv2 = sign._hash_password(plain, salt)
     assert rv1 == rv2
 
 def test_signup_hash_password_reverse():
-    salt = base64.b64encode(os.urandom(32)) 
+    salt = base64.b64encode(os.urandom(32))
     plain = 'test-password2'
     password = sign._hash_password(plain, salt)
     rv = sign._hash_check(plain, password)
